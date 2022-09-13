e16a9072aba93d1131d0f54755801f9ebdb33549
Fabio Lenon <fabio.lenon.fls@gmail.com>
2022-09-12 21:08:44 -0300

    parte 1

--- damas.js
+++ damas.js
@@ -18,3 +18,87 @@ function imprimirTabuleiro(tabuleiro) {
 
     console.table(tabela);
 }
+
+lxcParaIndex(lxc) {
+    if (lxc !== null) {
+        let lc = lxc.split('x').map(Number);
+        let l = lc[o];
+            c = lc[1];
+
+        if (0 <= l && l < 8 && 0 <= c && c < 8) {
+            return l * 8 + c;
+        }
+    }
+
+    return null;
+}
+
+function indexParaLxC(index) {
+    if (index < 0 || index >= 64) {
+        return null;
+    }
+
+    let l = Math.floor(index / 8);
+        c = index % 8;
+
+        return l + 'x' + c;
+}
+
+function escolherPeca(tabuleiro, cor) {
+    let index;
+    do {
+        let lxc = prompt("Escolha a posição LxC de uma peça " + cor);
+        index = lxcParaIndex(lxc);
+    } while (index === null || tabuleiro[index].toLowerCase() !== cor[0])
+
+    return index;
+}
+
+function escolherLxCDestino(tabuleiro) {
+    let index;
+
+    do {
+        let lxc = prompt("Escolha uma casa vazia");
+        index = lxcParaIndex(lxc);
+    } while (index === null || tabuleiro[index] !== ' ')
+
+    return index;
+}
+
+function mover(tabuleiro, indexOrigem, indexDestino) {
+    let peca = tabuleiro[indexOrigem];
+    let diferenca = indexDestino - indexOrigem;
+
+    if (diferenca % 7 !== 0 && diferenca % 9 !== 0) {
+        return {ok: false, msg: "Movimento não-diagonal inválido"};
+    }
+
+    if (['p', 'b'].includes(peca)) {
+
+    }
+    else {
+
+    }
+}
+
+function promoverPecas(tabuleiro) {
+    let linhaInicio, linhaFim;
+
+    linhaInicio = 1;
+    linhaFim = 7;
+    for (let i = linhaInicio; i <= linhaFim; i += 2) {
+        if (tabuleiro[i] === 'b') {
+            tabuleiro[i] = 'B';
+        }
+    }
+
+    linhaInicio = 7*8;
+    linhaFim = linhaInicio + 7;
+    for (let i = linhaInicio; i <= linhaFim; i += 2) {
+        if (tabuleiro[i] === 'p') {
+            tabuleiro[i] = 'P';
+        }
+    }
+}
+
+// PAREI NO TEMPO 46 MINUTOS DO VIDEO
